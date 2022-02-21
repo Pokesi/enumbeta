f5e41feebae9f9f6561fa63a62deddcf11d2434e
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-02-21 14:11:36 +1100

    Update getApiInfo.js

--- src/features/helpers/getApiInfo.js
+++ src/features/helpers/getApiInfo.js
@@ -1,5 +1,5 @@
-export const apiUrl = 'https://api.grim.finance';
-
+export const apiUrl = 'https://api.beefy.finance';
+export const backUpUrl = 'https://api.grim.finance';
 // Time-based cache buster
 export const getApiCacheBuster = () => {
   return Math.trunc(Date.now() / (1000 * 60));
